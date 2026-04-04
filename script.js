fd1738ecc98557bae50c6c5ea2291d940e275e4b
Katrina Moody <kmfoster212@gmail.com>
2026-04-03 22:17:42 -0400

    Added new images, deleted some old images, rearranged to look good on the website.

--- script.js
+++ script.js
@@ -11,12 +11,25 @@ function myFunction() {
 // To swap images: edit this list only — add, remove, or rename filenames.
 
 const galleryImages = [
+  "448357791_10226322293265541_587783287595115038_n.jpg",
   "429857080_10225815935206906_7507094253980350770_n.jpg",
   "429898171_10225815933766870_3324925333662239892_n.jpg",
-  "430023519_10225824206293678_1682576699052334100_n.jpg",
-  "430140149_10225824206853692_5892372674248940475_n.jpg",
-  "445488653_10226244446439419_7282085078570979241_n.jpg",
-  "448357791_10226322293265541_587783287595115038_n.jpg",
+  "IMG_8366.PNG",
+  "IMG_8343.PNG",
+  "unnamed (8).jpg",
+  "unnamed (12).jpg",
+  "IMG_8367.PNG",
+  "unnamed (16).jpg",
+  "unnamed (18).jpg",
+  "unnamed (20).jpg",
+  "unnamed (23).jpg",
+  "unnamed (26).jpg",
+  "unnamed (31).jpg",
+  "unnamed (33).jpg",
+  "unnamed (35).jpg",
+  "unnamed (1).jpg",
+  "unnamed (37).jpg",
+  "unnamed (38).jpg",
   "IMG_8321.PNG",
   "IMG_8322.PNG",
   "IMG_8323.JPG",
@@ -25,27 +38,22 @@ const galleryImages = [
   "IMG_8329.PNG",
   "IMG_8330.PNG",
   "IMG_8331.PNG",
-  "IMG_8334.PNG",
-  "IMG_8335.PNG",
   "IMG_8337.PNG",
   "IMG_8340.PNG",
   "IMG_8341.PNG",
-  "IMG_8343.PNG",
+  "unnamed (2).jpg",
   "IMG_8345.PNG",
   "IMG_8346.PNG",
+  "unnamed (36).jpg",
   "IMG_8347.PNG",
   "IMG_8350.PNG",
   "IMG_8354.PNG",
   "IMG_8355.PNG",
-  "IMG_8356.PNG",
   "IMG_8357.PNG",
   "IMG_8358.PNG",
   "IMG_8359.PNG",
-  "IMG_8361.PNG",
-  "IMG_8363.PNG",
-  "IMG_8365.PNG",
-  "IMG_8366.PNG",
-  "IMG_8367.PNG",
+  "430140149_10225824206853692_5892372674248940475_n.jpg",
+  "430023519_10225824206293678_1682576699052334100_n.jpg",
 ];
 
 function renderGallery() {
@@ -57,7 +65,7 @@ function renderGallery() {
     const div = document.createElement("div");
     const img = document.createElement("img");
     img.src = `images/gallery/${filename}`;
-    img.alt = "Hair by Glinda - hair styling work";
+    img.alt = "Hair style work by Glinda";
     div.appendChild(img);
     container.appendChild(div);
   });
